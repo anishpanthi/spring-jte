d2f405af029167e1ebd1f32ac6bb321004246b86
Anish <anish.panthi41@gmail.com>
2024-10-26 13:55:10 -0500

    Initial Commit

---
+++ src/test/java/dev/app/AppJteApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppJteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
